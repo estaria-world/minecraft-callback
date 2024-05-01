960c094926b4e19273cde7e2f349410655325fc7
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-05-01 19:46:10 +0200

    update paper-command-kit dependency

--- callback-plugin/plugin-paper/build.gradle.kts
+++ callback-plugin/plugin-paper/build.gradle.kts
@@ -5,8 +5,7 @@ dependencies {
     api(project(":callback-api"))
 
     // estaria dependencies
-    compileOnly("world.estaria:paper-command-kit:1.0.5")
-    compileOnly("world.estaria:github-file-manager:1.0.1")
+    compileOnly("world.estaria:paper-command-kit:1.0.6")
 
     // paper dependencies
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
